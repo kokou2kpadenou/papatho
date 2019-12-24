3c61e619ab2f50393b27e5ab040ee62ab488b15f
kokou kpadenou <kkokou@gmail.com>
2019-12-23 20:42:38 -0500

    add store

---
+++ src/store/configureStore.js
@@ -0,0 +1,24 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import rootReducer from "../reducers";
+import socketMiddleware from "../middleware/socketMiddleware";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["connected"]
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default () => {
+  let store = createStore(
+    persistedReducer,
+    applyMiddleware(socketMiddleware(), thunk)
+  );
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
