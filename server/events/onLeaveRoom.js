5e8e71d3d9e1e59b0b1f87c8f604f9795d29ed0a
kokou kpadenou <kkokou@gmail.com>
2020-01-03 10:50:40 -0500

    leave room event

---
+++ server/events/onLeaveRoom.js
@@ -0,0 +1,64 @@
+const ObjectId = require("mongoose").Types.ObjectId;
+const generateAlertMessage = require("../others/generateAlertMessage");
+
+// import all the model
+const Messages = require("../models/messages");
+const Users = require("../models/users");
+const Rooms = require("../models/rooms");
+
+const onLeaveRoom = (io, socket) => {
+  socket.on("leave-room", ({ userName, room }) => {
+    Messages.find({ room: room })
+      .then(messages => {
+        const roomMessages = messages.map(({ _id }) => _id);
+
+        Users.findOneAndUpdate(
+          { user: userName },
+          {
+            $pull: { rooms: room, messages: { message: { $in: roomMessages } } }
+          }
+        )
+          .then(user => {
+            socket.leave(room);
+            socket.broadcast.to(room).emit(
+              "message",
+              generateAlertMessage({
+                room: room,
+                text: `${userName} left`
+              })
+            );
+
+            /** Additional process for room info */
+            // Remove user from joinedUsers and onlineUsers and typing
+            Rooms.findOneAndUpdate(
+              { _id: ObjectId(room) },
+              {
+                $pull: {
+                  joinedUsers: userName,
+                  onlineUsers: userName,
+                  typingUsers: userName
+                }
+              }
+            )
+              .then(res => {
+                // Send the room new info details to all clients in the room
+                Rooms.find({ _id: ObjectId(room) })
+                  .then(roomInfo => {
+                    io.in(room).emit("update-room", roomInfo);
+                  })
+                  .catch(err => {
+                    console.log(err);
+                  });
+              })
+              .catch(err => {
+                console.log(err);
+              });
+            /** End additional process for room info */
+          })
+          .catch(err => console.log(err));
+      })
+      .catch(err => console.log(err));
+  });
+};
+
+module.exports = onLeaveRoom;
