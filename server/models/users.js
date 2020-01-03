67d035521413aec4934070f19a403459997855bb
kokou kpadenou <kkokou@gmail.com>
2020-01-02 21:57:39 -0500

    update users model

--- server/models/users.js
+++ server/models/users.js
@@ -4,9 +4,11 @@ const Schema = mongoose.Schema;
 const usersSchema = Schema({
   _id: Schema.Types.ObjectId,
   user: String,
+  socketID: [String],
   rooms: [{ type: Schema.Types.ObjectId, ref: "Rooms" }],
   messages: [
     {
+      _id: false,
       message: { type: Schema.Types.ObjectId, ref: "Messages" },
       status: String
     }
