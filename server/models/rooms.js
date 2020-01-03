f986ca975f5302c2ba21ced5efd89e36836b7607
kokou kpadenou <kkokou@gmail.com>
2020-01-02 21:57:14 -0500

    update rooms model

--- server/models/rooms.js
+++ server/models/rooms.js
@@ -5,7 +5,14 @@ const roomsSchema = new Schema({
   _id: Schema.Types.ObjectId,
   roomName: String,
   roomDesc: String,
-  roomOwner: String
+  roomOwner: String,
+  joinedUsers: [String],
+  onlineUsers: [String],
+  typingUsers: [String]
+});
+
+roomsSchema.static("findCommonRoom", function() {
+  return this.find({ roomName: "COMMON" });
 });
 
 module.exports = mongoose.model("Rooms", roomsSchema);
