cfe840576af7390807769a790860d0de9a459798
kokou kpadenou <kkokou@gmail.com>
2020-01-03 10:57:37 -0500

    typing event

---
+++ server/events/onTyping.js
@@ -0,0 +1,35 @@
+const ObjectId = require("mongoose").Types.ObjectId;
+
+const Rooms = require("../models/rooms");
+
+const onTyping = (io, socket) => {
+  socket.on("typing", ({ userName, room }) => {
+    /** Additional process for room info */
+    // Add user to joinedUsers and onlineUsers
+    Rooms.findOneAndUpdate(
+      { _id: ObjectId(room) },
+      {
+        $addToSet: {
+          typingUsers: userName
+        }
+      }
+    )
+      .then(res => {
+        // Send the room new info details to all clients in the room
+        Rooms.find({
+          _id: ObjectId(room)
+        })
+          .then(roomInfo => {
+            io.in(room).emit("update-room", roomInfo);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+};
+
+module.exports = onTyping;
