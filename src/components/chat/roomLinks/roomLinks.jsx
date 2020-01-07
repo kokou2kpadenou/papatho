63393ff9310e1fc51b68036e2a8a93ccb09a3019
kokou kpadenou <kkokou@gmail.com>
2020-01-07 17:52:05 -0500

    roomLinks component reorganized

---
+++ src/components/chat/roomLinks/roomLinks.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import RoomLink from "../../common/roomLink/roomLink";
+import "./roomLinks.css";
+
+export default ({ show, joinedRooms, showListRooms }) => (
+  <div className="nav-rooms">
+    <div className="nav-rooms__show-button" onClick={showListRooms}>
+      {show ? (
+        <>
+          <span>↑</span>
+          <span className="nav-rooms__label">Show Rooms Lists</span>
+          <span>↑</span>
+        </>
+      ) : (
+        <>
+          <span>↓</span>
+          <span className="nav-rooms__label">Hide Rooms List</span>
+          <span>↓</span>
+        </>
+      )}
+    </div>
+    <div className={`nav-rooms__list ${show ? "show" : ""}`}>
+      <RoomLink roomName="ROOMS" link={true}>
+        ROOMS
+      </RoomLink>
+      {joinedRooms.map(joinedRoom => (
+        <RoomLink
+          key={joinedRoom._id}
+          roomName={joinedRoom.roomName}
+          nav={true}
+          rootPath="/rooms/"
+          newMessage={joinedRoom.newMessages}
+        />
+      ))}
+    </div>
+  </div>
+);
