728dada383f1a0a456d5675a4b9f19f537e1ce50
kokou kpadenou <kkokou@gmail.com>
2020-01-09 19:18:18 -0500

    messages reducer final update

--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -1,23 +1,58 @@
-export const messages = (state = [], action) => {
-  switch (action.type) {
-    case "RESET_MESSAGES":
-      return [];
+const initialMessages = { currentRoom: "", messages: [] };
 
-    case "SET_MESSAGES":
-      return action.result;
+export const messages = (state = initialMessages, action) => {
+  switch (action.type) {
+    case "RESET_ALL":
+      return { currentRoom: "", messages: [] };
+
+    case "UPDATE_CURRENT_ROOM":
+      return { ...state, currentRoom: action.room };
+
+    case "RESET_MESSAGES":
+      return { ...state, messages: [] };
+
+    case "ADD_MESSAGES":
+      return { ...state, messages: action.result };
 
     case "ADD_MESSAGE":
-      return [...state, { ...action.message }];
+      console.log(state.currentRoom);
+
+      return {
+        ...state,
+        messages: [
+          ...state.messages,
+          state.currentRoom === action.result.room &&
+          action.result.status === "NEW"
+            ? { ...action.result, status: "VIEW" }
+            : action.result
+        ]
+      };
+
+    case "UPDATE_MESSAGE":
+      return {
+        ...state,
+        messages: state.messages.map(message =>
+          message.ref === action.result.ref ? action.result : message
+        )
+      };
 
     case "REMOVE_MESSAGES":
-      return state.filter(message => message.room !== action.room);
+      return {
+        ...state,
+        messages: state.messages.filter(
+          message => message.room !== action.result
+        )
+      };
 
     case "CHANGE_STATUS":
-      return state.map(message =>
-        !message.view && message.room === action.room
-          ? { ...message, status: action.status }
-          : message
-      );
+      return {
+        ...state,
+        messages: state.messages.map(message =>
+          message.status === "NEW" && message.room === action.result.room
+            ? { ...message, status: action.result.status }
+            : message
+        )
+      };
 
     default:
       return state;
