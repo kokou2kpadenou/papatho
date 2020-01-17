2048a3bffbd2e6eab2528374f0e67b347ad92f68
kokou kpadenou <kkokou@gmail.com>
2020-01-17 18:16:19 -0500

    join component updated for new user structure

--- src/components/join/join.jsx
+++ src/components/join/join.jsx
@@ -1,15 +1,47 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
+import Icon from "../common/icon/icon";
+import ConfirmationDlg from "../common/confirmationDlg/confirmationDlg";
 import "./join.css";
 
-const Join = ({ rooms, user, connected, emit }) => {
+import { autoGenID } from "../../helper/index";
+
+const Join = ({
+  rooms,
+  currentUser,
+  newUser,
+  userRef,
+  connected,
+  emit,
+  cancelUserCreation
+}) => {
   const [name, setName] = useState("");
 
   const _onClick = e => {
     e.preventDefault();
     if (name) {
-      emit({ emit: "join-chat", payload: name, handle: "UPDATE_USER" });
+      emit({
+        emit: "join-chat",
+        payload: { userName: name, ref: autoGenID("user") },
+        handle: "UPDATE_USER_REF"
+      });
     }
+  };
+
+  const _action = () => {
+    //
+    if (name) {
+      emit({
+        emit: "create-user",
+        payload: { userName: name, ref: userRef },
+        handle: "UPDATE_USER_REF"
+      });
+    }
+  };
+
+  const _cancel = () => {
+    cancelUserCreation();
+    setName("");
   };
 
   const checkRoomsLoaded = () => {
@@ -21,29 +53,44 @@ const Join = ({ rooms, user, connected, emit }) => {
     return false;
   };
 
-  if (user && checkRoomsLoaded()) {
+  const dlgMsg = `It is the first time for ${name}. Do you want to continue and create new user as ${name}?`;
+
+  if (currentUser && checkRoomsLoaded()) {
     return <Redirect to="/rooms/COMMON" />;
   }
 
   return (
     <div className="join__container">
+      {userRef && newUser && (
+        <ConfirmationDlg actionFn={_action} cancelFn={_cancel}>
+          {dlgMsg}
+        </ConfirmationDlg>
+      )}
       <form className="join__content">
         <div>
           {!connected && <p style={{ color: "red" }}>Network Issue</p>}
+          {userRef && !newUser && <p>Trying to retrieve data...</p>}
+          <Icon style={{ width: "10rem", height: "10rem" }} />
           <h1 className="join__title">PAPATHO</h1>
           <input
             autoFocus
+            disabled={userRef && newUser}
             className="join__input"
-            placeholder="Username"
-            aria-label="Name"
+            placeholder="USERNAME"
+            aria-label="USERNAME"
             type="text"
-            onChange={e => setName(e.target.value)}
+            value={name}
+            minLength="3"
+            maxLength="8"
+            onChange={e =>
+              setName(e.target.value.replace(/[^\w]/gi, "").toUpperCase())
+            }
           />
         </div>
         <button
           className="join__btn"
           type="submit"
-          disabled={!name || !connected}
+          disabled={!name || name.length < 3 || (userRef && newUser)}
           onClick={e => _onClick(e)}
         >
           Join
--- src/containers/join.js
+++ src/containers/join.js
@@ -1,15 +1,19 @@
 import { connect } from "react-redux";
-import { emit } from "../actions";
+import { emit, cancelUserCreation } from "../actions";
 import Join from "../components/join/join";
 
 const mapStateToProps = state => ({
   rooms: state.rooms,
-  user: state.user,
+  currentUser: state.user.currentUser,
+  newUser: state.user.newUser,
+  userRef: state.user.ref,
+  nbOfAttent: state.user.count,
   connected: state.connected
 });
 
 const mapDispatchToProps = dispatch => ({
-  emit: data => dispatch(emit(data))
+  emit: data => dispatch(emit(data)),
+  cancelUserCreation: () => dispatch(cancelUserCreation())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Join);
