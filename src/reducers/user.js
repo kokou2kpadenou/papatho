c2f114bdb3cb604fe6fcad5c92c17676d3ef01ea
kokou kpadenou <kkokou@gmail.com>
2020-01-17 18:06:10 -0500

    reducer user modified to support the new user structure

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,10 +1,40 @@
-export const user = (state = "", action) => {
+const initialUser = { currentUser: "", newUser: "", ref: "", count: 0 };
+
+export const user = (state = initialUser, action) => {
   switch (action.type) {
-    case "UPDATE_USER":
-      return action.result;
+    case "UPDATE_CURRENT_USER":
+      return action.result.ref === state.ref
+        ? {
+            currentUser: action.result.userName,
+            newUser: "",
+            ref: "",
+            count: 0
+          }
+        : initialUser;
+
+    case "UPDATE_NEW_USER":
+      return action.result.ref === state.ref
+        ? {
+            currentUser: "",
+            newUser: action.result.userName,
+            ref: action.result.ref,
+            count: state.count + 1
+          }
+        : initialUser;
+
+    case "UPDATE_USER_REF":
+      return {
+        currentUser: "",
+        newUser: "",
+        ref: action.result.ref,
+        count: state.count + 1
+      };
+
+    case "CLEAR_USER":
+      return initialUser;
 
     case "RESET_ALL":
-      return "";
+      return initialUser;
 
     default:
       return state;
