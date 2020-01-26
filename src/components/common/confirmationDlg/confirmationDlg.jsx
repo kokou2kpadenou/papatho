014753a4a47a9284149373138d35e7c595b94840
kokou kpadenou <kkokou@gmail.com>
2020-01-25 22:57:54 -0500

    confirmationDlg changes after dialog extraction

--- src/components/common/confirmationDlg/confirmationDlg.jsx
+++ src/components/common/confirmationDlg/confirmationDlg.jsx
@@ -1,31 +1,23 @@
 import React from "react";
 import Button from "../button/button";
+import Dialog from "../dialog/dialog";
 import "./confirmationDlg.css";
 
-export default ({ children, actionFn, cancelFn }) => {
+export default ({ children, actionFn, cancelFn, dlgType, dlgTitle }) => {
   const _action = e => {
     e.preventDefault();
     actionFn();
   };
 
-  const _cancel = e => {
-    if (e.target === e.currentTarget) {
-      cancelFn();
-    }
-  };
-
   return (
-    <div className="dlg__container" onClick={e => _cancel(e)}>
-      <div className="dlg__form">
-        <div className="dlg__icon"> &#9888;</div>
-        <div className="dlg__msg">{children}</div>
-        <div className="dlg__buttons">
-          <Button onClick={e => _action(e)}>Yes</Button>
-          <Button autoFocus onClick={cancelFn}>
-            No
-          </Button>
-        </div>
+    <Dialog cancelFn={cancelFn} dlgType={dlgType} dlgTitle={dlgTitle}>
+      <div className="dlg__msg">{children}</div>
+      <div className="dlg__buttons">
+        <Button autoFocus onClick={cancelFn}>
+          No
+        </Button>
+        <Button onClick={e => _action(e)}>Yes</Button>
       </div>
-    </div>
+    </Dialog>
   );
 };
