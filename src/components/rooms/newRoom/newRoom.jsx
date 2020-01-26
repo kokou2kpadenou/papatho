85bced3b4f8eb7d43f5ec4d11b6d7667b7544b72
kokou kpadenou <kkokou@gmail.com>
2020-01-26 11:32:22 -0500

    newRoom after dialog extraction

--- src/components/rooms/newRoom/newRoom.jsx
+++ src/components/rooms/newRoom/newRoom.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from "react";
 import Button from "../../common/button/button";
+import Dialog from "../../common/dialog/dialog";
 import "./newRoom.css";
 import { autoGenID } from "../../../helper";
 
@@ -34,59 +35,49 @@ export default ({ user, rooms, emit, setShowNewRoom }) => {
     _Clear();
   };
 
-  const _cancel = e => {
-    if (e.target === e.currentTarget) {
-      _Clear();
-    }
-  };
-
   const _exist = () => {
     return rooms.filter(room => room.roomName === roomName).length > 0;
   };
 
-  console.log(_exist());
-
   return (
-    <div className="newroom_container" onClick={e => _cancel(e)}>
-      <div className="newroom_form">
-        <div className={_exist() ? "msg-error" : ""}>
-          <input
-            ref={ref}
-            className="newroom__input"
-            placeholder="Room Name"
-            aria-label="Room Name"
-            type="text"
-            value={roomName}
-            minLength="2"
-            maxLength="6"
-            onChange={e =>
-              setRoomName(e.target.value.replace(/[^\w]/gi, "").toUpperCase())
-            }
-          />
-        </div>
-
-        <div>
-          <input
-            className="newroom__input"
-            placeholder="Room Description"
-            aria-label="Room Description"
-            type="text"
-            value={roomDesc}
-            onChange={e => setRoomDesc(e.target.value)}
-          />
-        </div>
-        <div className="newroom__buttons">
-          <Button
-            type="submit"
-            onClick={e => _AddRoom(e)}
-            disabled={!roomName || !roomDesc || roomName.length < 2 || _exist()}
-          >
-            Add
-          </Button>
-
-          <Button onClick={_Clear}>Cancel</Button>
-        </div>
+    <Dialog cancelFn={_Clear} dlgType="group" dlgTitle="New Room">
+      <div className={_exist() ? "msg-error" : ""}>
+        <input
+          ref={ref}
+          className="newroom__input"
+          placeholder="Room Name (min: 2 - max: 6)"
+          aria-label="Room Name"
+          type="text"
+          value={roomName}
+          minLength="2"
+          maxLength="6"
+          onChange={e =>
+            setRoomName(e.target.value.replace(/[^\w]/gi, "").toUpperCase())
+          }
+        />
       </div>
-    </div>
+
+      <div>
+        <input
+          className="newroom__input"
+          placeholder="Room Description"
+          aria-label="Room Description"
+          type="text"
+          value={roomDesc}
+          onChange={e => setRoomDesc(e.target.value)}
+        />
+      </div>
+      <div className="newroom__buttons">
+        <Button
+          type="submit"
+          onClick={e => _AddRoom(e)}
+          disabled={!roomName || !roomDesc || roomName.length < 2 || _exist()}
+        >
+          Add
+        </Button>
+
+        <Button onClick={_Clear}>Cancel</Button>
+      </div>
+    </Dialog>
   );
 };
